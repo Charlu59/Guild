68199e0fe87d8bc197148ddd04d2c565e268dafb
Charlu59 <charles-hubert_basuiau@hotmail.fr>
2016-05-11 14:14:02 +0200

    change project

---
+++ Jenkins/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jenkins
+//
+//  Created by Charles-Hubert Basuiau on 11/05/2016.
+//  Copyright © 2016 Appiway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Jenkins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jenkins
+//
+//  Created by Charles-Hubert Basuiau on 11/05/2016.
+//  Copyright © 2016 Appiway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
